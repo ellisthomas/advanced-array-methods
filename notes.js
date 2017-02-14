dbd511a004640a8c896ef4e2ce446c62c72071b7
ellisthomas <ellisthomas@gmail.com>
2017-02-13 20:09:31 -0600

    pop quiz

--- notes.js
+++ notes.js
@@ -97,6 +97,9 @@ var fruits = ["apple", "banana", "cherry"];
 // });
 // console.log(sum);
 
+
+
+
 // Using one single line of JavaScript code, complete the following tasks on the array of integers below.
 
 // 1.  Sort the numbers in descending order (10, 9, 8, 7, etc).
@@ -105,6 +108,15 @@ var fruits = ["apple", "banana", "cherry"];
 // 4.  Then output (either in the DOM or the console) the sum of all the resulting numbers.
 
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+var chainingResult = integers.sort(function(a,b){return b-a}).filter(function(num){
+	return num < 19}).map(function(num){return (num *1.5)-1}).reduce(function(prev, curr){
+		return prev + curr
+	});
+
+console.log("Result ", chainingResult);
+
+
 
 
 
